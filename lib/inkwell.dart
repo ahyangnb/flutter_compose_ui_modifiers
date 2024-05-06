4ad13e2359633293de3aa067a336947b439f0817
zengyang <xxx@xxx.xx>
2024-05-06 19:20:13 +0800

    0.3.5 optimize code.

--- lib/inkwell.dart
+++ lib/inkwell.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_compose_ui_modifiers/flutter_compose_ui_modifiers.dart';
 
 extension FlutterComposeUiModifierEvent on Widget {
   InkWell click(VoidCallback onTap) {
