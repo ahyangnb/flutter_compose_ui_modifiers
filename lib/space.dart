ef68a21193d10607b059166b928ddcf155865fba
zengyang <xxx@xxx.xx>
2024-05-07 11:16:12 +0800

    spaceHeight etc.

--- lib/space.dart
+++ lib/space.dart
@@ -4,4 +4,20 @@ extension SpaceNum on num {
   SizedBox get space {
     return SizedBox(height: this.toDouble(), width: this.toDouble());
   }
+
+  SizedBox get spaceH {
+    return SizedBox(height: this.toDouble());
+  }
+
+  SizedBox get spaceW {
+    return SizedBox(width: this.toDouble());
+  }
+
+  SizedBox get spaceHeight {
+    return SizedBox(height: this.toDouble());
+  }
+
+  SizedBox get spaceWidth {
+    return SizedBox(width: this.toDouble());
+  }
 }
