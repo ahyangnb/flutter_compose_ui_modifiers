7c6436a5bf384482ce9a9caaf720f31db0c33fba
ahyangnb <zonggeu@gmail.com>
2024-07-24 15:57:56 +0800

    language exampple

--- lib/other/m_reverse.dart
+++ lib/other/m_reverse.dart
@@ -3,7 +3,36 @@ import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-// for ARABIC language.
+/// for ARABIC language.
+/// When set language please refer to:
+/// example
+/// ```dart
+// static Locale get currentLocale {
+//   logger.d(
+//     '[currentLocale] Get.locale::${Get.locale}, '
+//         'window.locale::${window.locale}, '
+//         'Platform.localeName::${Platform.localeName}',
+//   );
+//   final String? languageSave =
+//   GetStorage().read<String>(SpKey.languageSaveKey);
+//   Locale getLocale;
+//   if (languageSave == null) {
+//     if (window.locale.languageCode == 'zh') {
+//       /// Only use traditional mandarin.
+//       getLocale = LanguageEnumType.CHINESE.locale;
+//     } else if (window.locale.languageCode == 'ars') {
+//       /// Najdi Arabic[ars as language code.]
+//       getLocale = LanguageEnumType.ARABIC.locale;
+//     } else {
+//       getLocale = window.locale;
+//     }
+//   } else {
+//     getLocale = Locale(languageSave);
+//   }
+//   Get.locale = getLocale;
+//   return getLocale;
+// }
+/// ```
 class MReverseWidget extends StatelessWidget {
   const MReverseWidget({required this.child, this.reverse, super.key});
 
