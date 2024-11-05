f9c02104b798cd568cd2a2fa849901e3010990d2
ahyangnb <zonggeu@gmail.com>
2024-11-05 11:31:38 +0800

    mShowBigPicture add herotag

--- lib/other/m_picture.dart
+++ lib/other/m_picture.dart
@@ -5,21 +5,43 @@ import 'package:flutter_compose_ui_modifiers/flutter_compose_ui_modifiers.dart';
 import 'package:get/get.dart';
 import 'package:photo_view/photo_view.dart';
 
-Future<void>? mShowBigPicture(String? url) async {
+Future<void>? mShowBigPicture(String? url, {final Object? heroTag}) async {
   if (url!.isEmpty) {
     myToast("The picture url is empty.");
     return;
   }
-  return Get.to<void>(MBigPicPage(url));
+  return Get.to<void>(MBigPicPage(url, heroTag));
 }
 
 class MBigPicPage extends StatelessWidget {
   final String url;
+  final Object? heroTag;
 
-  MBigPicPage(this.url);
+  MBigPicPage(this.url, this.heroTag);
 
   @override
   Widget build(BuildContext context) {
+    final img = PhotoView(
+      imageProvider: mGetImageProvider(url),
+      initialScale: PhotoViewComputedScale.contained,
+      heroAttributes: PhotoViewHeroAttributes(tag: url),
+      minScale: 0.3,
+      maxScale: 5.0,
+      onTapUp: (BuildContext context, TapUpDetails details,
+              PhotoViewControllerValue controllerValue) =>
+          Get.back(),
+      loadingBuilder: (BuildContext context, ImageChunkEvent? event) => Center(
+        child: SizedBox(
+          width: 20.0,
+          height: 20.0,
+          child: CircularProgressIndicator(
+            value: event == null
+                ? 0
+                : event.cumulativeBytesLoaded / (event.expectedTotalBytes ?? 0),
+          ),
+        ),
+      ),
+    );
     return Stack(
       children: [
         AppBar(systemOverlayStyle: SystemUiOverlayStyle.light),
@@ -28,29 +50,7 @@ class MBigPicPage extends StatelessWidget {
           // systemUiOverlayStyle: SystemUiOverlayStyle.light,
           body: Stack(
             children: <Widget>[
-              PhotoView(
-                imageProvider: mGetImageProvider(url),
-                initialScale: PhotoViewComputedScale.contained,
-                heroAttributes: PhotoViewHeroAttributes(tag: url),
-                minScale: 0.3,
-                maxScale: 5.0,
-                onTapUp: (BuildContext context, TapUpDetails details,
-                        PhotoViewControllerValue controllerValue) =>
-                    Get.back(),
-                loadingBuilder:
-                    (BuildContext context, ImageChunkEvent? event) => Center(
-                  child: SizedBox(
-                    width: 20.0,
-                    height: 20.0,
-                    child: CircularProgressIndicator(
-                      value: event == null
-                          ? 0
-                          : event.cumulativeBytesLoaded /
-                              (event.expectedTotalBytes ?? 0),
-                    ),
-                  ),
-                ),
-              ),
+              if (heroTag != null) Hero(tag: heroTag!, child: img) else img,
               Positioned(
                 left: 0.px,
                 top: 0.px,
