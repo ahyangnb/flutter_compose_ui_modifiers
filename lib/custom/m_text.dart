e66fb6da1e75a4c755688b0a5a5799a0c22894e7
ahyangnb <zonggeu@gmail.com>
2024-11-06 16:52:21 +0800

    support defText in MTextController

--- lib/custom/m_text.dart
+++ lib/custom/m_text.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_compose_ui_modifiers/flutter_compose_ui_modifiers.dart';
+import 'package:get/get.dart';
 
 class MTextController extends TextEditingController {
   final String? name;
@@ -8,7 +9,14 @@ class MTextController extends TextEditingController {
 
   FocusNode focusNode = FocusNode();
 
-  MTextController({this.name, required this.maxInput, this.mustInput = true}) {
+  MTextController(
+      {this.name,
+      required this.maxInput,
+      this.mustInput = true,
+      String? defText}) {
+    if (GetUtils.isNullOrBlank(defText)?.not() ?? false) {
+      text = defText!;
+    }
     this.addListener(() {
       if (this.text.length > maxInput) {
         this.text = this.text.substring(0, maxInput);
